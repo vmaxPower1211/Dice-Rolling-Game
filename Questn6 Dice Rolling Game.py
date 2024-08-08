2567e91efffe9268d4409dd6ee5497801e8f3906
unknown <vamxPower1211@gmail.com>
2024-08-08 00:46:30 -0700

    dice complete

---
+++ Questn6 Dice Rolling Game.py
@@ -0,0 +1,24 @@
+#Project 6
+#Create	a dice rolling game
+#Won’t need user input this time if you	don’t want (not	necessary)
+#Think	about a	while loop maybe?
+#Seems like a good place for an	IF and ELSE statement
+
+print("--------------------------------------------------------")
+
+import random
+
+question = input('Would you like to play, [y/n]? ')
+
+while question != "n":
+    if question == "y":
+        die1 = random.randint(1, 6)
+        die2 = random.randint(1, 6)
+        print("Your rolled a...")
+        print(die1," and ",die2)
+        question = input('\nWould you like to roll again, [y/n]? ')
+    else:
+        print('Invalid response. Please type "y" or "n".')
+        question = input('Would you like to try to roll the dice again[y/n]?\n ')        
+
+print('Thank you for playing - Good Bye!')
